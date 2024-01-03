851987828a4481c70d632c5e469252dba9fd2a88
Mario <mh.torneria@gmail.com>
2024-01-02 21:31:02 -0300

    .

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,28 +5,28 @@ import React from 'react';
 
 function NavBar(){
     return(
-<nav class="navbar navbar-expand-sm align-items-center bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#home"><h2>MapAventuras</h2></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#Home">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#About">Acerca de</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#Contacto">Contacto</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-bg-light" href="#Salir">Salir</a>
-        </li>
-      </ul>
-    </div>
+<nav className="navbar sticky-top navbar-expand-sm align-items-center bg-dark navbar-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#home"><h2>MapAventuras</h2></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justift-content-end d-flex" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <a className="nav-link" href="#Home">Inicio</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#About">Acerca de</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#Contacto">Contacto</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-bg-light" style={{borderRadius:10}}  href="#Salir">Salir</a>
+                </li>
+            </ul>
+        </div>
   </div>
 </nav>
     );
