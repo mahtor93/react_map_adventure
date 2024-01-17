df86e2d89730c7a6f6813bd639631c8ee7a5c903
Mario <mh.torneria@gmail.com>
2024-01-17 15:33:48 -0300

    corrección de integración de datos en el menú select

--- src/components/utilities/zoneDropdown.js
+++ src/components/utilities/zoneDropdown.js
@@ -24,7 +24,7 @@ const DropdownZones=()=> {
         }
     },[zonasFetched])
     
-    const zonasFlat = zonas.flat();
+    const zonasFlat = Array.isArray(zonas) ? zonas.flat() : [];
 
     return(
 <div>
